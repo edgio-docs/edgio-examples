4f67b53e8aa312d0aea25071cf8f8286eb4d1a59
Tristan Lee <tristan.lee@moovweb.com>
2022-11-16 21:49:05 -0500

    Reduce config for testing

--- herding-cats.config.js
+++ herding-cats.config.js
@@ -4,5 +4,21 @@ module.exports = {
       repo: 'git@github.com:edgio-docs/edgio-angular-example.git',
       branch: 'master',
     },
+    {
+      repo: 'git@github.com:edgio-docs/edgio-astro-example.git',
+      branch: 'main',
+    },
+    {
+      repo: 'git@github.com:edgio-docs/edgio-astro-ssr-example.git',
+      branch: 'main',
+    },
+    {
+      repo: 'git@github.com:edgio-docs/edgio-aws-store-example.git',
+      branch: 'master',
+    },
+    {
+      repo: 'git@github.com:edgio-docs/edgio-brunch-example.git',
+      branch: 'main',
+    },
   ],
 };
