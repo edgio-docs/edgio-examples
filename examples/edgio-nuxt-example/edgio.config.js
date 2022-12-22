186b2896729d4d5201365ed1788403ef0995349e
Tristan Lee <tristan.lee@moovweb.com>
2022-12-22 10:13:21 -0500

    Cleanup

--- examples/edgio-nuxt-example/edgio.config.js
+++ examples/edgio-nuxt-example/edgio.config.js
@@ -1,4 +1,4 @@
-// This file was automatically added by edgio init. Test
+// This file was automatically added by edgio init.
 // You should commit this file to source control.
 // Learn more about this file at https://docs.edgio.co/guides/edgio_config
 module.exports = {
