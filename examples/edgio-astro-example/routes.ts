e8616410f7635f980efa0994f66e96482910bd55
Tristan Lee <tristan.lee@moovweb.com>
2023-01-06 17:19:10 -0500

    Update all workflows

--- examples/edgio-astro-example/routes.ts
+++ examples/edgio-astro-example/routes.ts
@@ -5,14 +5,6 @@ import { astroRoutes } from '@edgio/astro'
 import { API_CACHE_HANDLER, IMAGE_CACHE_HANDLER } from './cache'
 
 export default new Router()
-  // Prevent search engines from indexing permalink URLs
-  .noIndexPermalink()
-  .match('/__xdn__/:path*', ({ redirect }) => {
-    redirect('/__layer0__/:path*', 301)
-  })
-  .match('/__layer0__/:path*', ({ redirect }) => {
-    redirect('/__edgio__/:path*', 301)
-  })
   .match('/l0-api/:path*', API_CACHE_HANDLER)
   .match('/l0-opt', IMAGE_CACHE_HANDLER)
   .use(astroRoutes)
