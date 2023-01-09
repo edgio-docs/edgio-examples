694e99fae404499ad831b8704d665d3cd34ddb3f
Tristan Lee <tristan.lee@moovweb.com>
2023-01-09 18:35:14 -0500

    Remove saber example since it fails to build due to vue dependency

--- examples/edgio-saber-example/sw/service-worker.js
+++
@@ -1,9 +0,0 @@
-import { Prefetcher } from '@edgio/prefetch/sw'
-import { precacheAndRoute } from 'workbox-precaching'
-import { skipWaiting, clientsClaim } from 'workbox-core'
-
-skipWaiting()
-clientsClaim()
-precacheAndRoute(self.__WB_MANIFEST || [])
-
-new Prefetcher().route()
