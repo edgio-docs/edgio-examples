e7a26b1a7722cb160777858749cdd266a2ab5d8f
Tristan Lee <tristan.lee@moovweb.com>
2023-01-13 12:52:55 -0500

    Update workflow to deploy from main or PR

--- scripts/helpers/getModifiedExamples.js
+++ scripts/helpers/getModifiedExamples.js
@@ -4,12 +4,21 @@ const examplesPath = 'examples';
 const examples = require('./getExamples')();
 
 module.exports = () => {
+  // Check if the current branch is `main`
+  const isMainBranch =
+    execSync('git rev-parse --abbrev-ref HEAD').toString().trim() === 'main';
+
   // Get the list of modified files
-  const modifiedFiles = execSync(
-    'git diff --name-only origin/main | xargs -I {} dirname {} | sort | uniq'
-  )
-    .toString()
-    .split('\n');
+  const modifiedFiles = isMainBranch
+    ? // If the current branch is `main`, get the list of modified files from the last commit
+      execSync(
+        'git diff --name-only HEAD~1 | xargs -I {} dirname {} | sort | uniq'
+      )
+    : execSync(
+        'git diff --name-only origin/main | xargs -I {} dirname {} | sort | uniq'
+      )
+        .toString()
+        .split('\n');
 
   // Filter the array of paths to return only the modified paths
   return examples.filter((path) =>
