f2000c79c98b1766491876faa40b4801f74ef1ff
Tristan Lee <tristan.lee@moovweb.com>
2023-01-17 11:44:27 -0500

    Update demo backend

--- examples/spartacus/edgio.config.js
+++ examples/spartacus/edgio.config.js
@@ -1,16 +1,16 @@
-'use strict'
+"use strict";
 
 // This file was automatically added by edgio deploy.
 // You should commit this file to source control.
 
 module.exports = {
-  connector: '@edgio/spartacus',
+  connector: "@edgio/spartacus",
   backends: {
     commerce: {
-      domainOrIp: 'spartacus-dev0.eastus.cloudapp.azure.com',
-      hostHeader: 'spartacus-dev0.eastus.cloudapp.azure.com',
-      port: 9002,
+      domainOrIp: "spartacus-demo.eastus.cloudapp.azure.com",
+      hostHeader: "spartacus-demo.eastus.cloudapp.azure.com",
+      port: 8443,
       disableCheckCert: true,
     },
   },
-}
+};
