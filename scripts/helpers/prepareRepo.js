1d7df97bd54baede36c3b52f2c7453f1d8b37ee2
Tristan Lee <tristan.lee@moovweb.com>
2023-01-18 20:21:27 -0500

    Add env vars

--- scripts/helpers/prepareRepo.js
+++ scripts/helpers/prepareRepo.js
@@ -1,24 +1,31 @@
 const { Octokit } = require('@octokit/rest');
+require('dotenv').config();
 
 const octokit = new Octokit({
   auth: process.env.GITHUB_API_TOKEN,
 });
 
 async function checkAndCreateRepo(repoUrl) {
+  const [owner, repo] = repoUrl.split(':')[1].split('/');
+  const repoName = repo.split('.')[0];
   try {
-    const [owner, repo] = repoUrl.split(':')[1].split('/');
     let { status } = await octokit.repos.get({
       owner,
-      repo: repo.split('.')[0],
+      repo: repoName,
     });
     if (status === 404) {
-      await octokit.repos.createForAuthenticatedUser({ name: repo });
+      await octokit.repos.createForAuthenticatedUser({ name: repoName });
       console.log(`Repository ${repoUrl} created`);
     }
     if (status !== 200 && status !== 201) {
       throw new Error(`Error checking repository ${repoUrl}`);
     }
   } catch (err) {
+    if (err.message === 'Not Found') {
+      await octokit.repos.createForAuthenticatedUser({ name: repoName });
+      console.log(`Repository ${repoUrl} created`);
+      return;
+    }
     throw new Error(`Error checking repository ${repoUrl}: ${err.message}`);
   }
 }
