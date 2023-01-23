ea1282a6fea8949ddb693713f20a8043d6efd44f
Tristan Lee <tristan.lee@moovweb.com>
2023-01-23 12:27:02 -0500

    Fix modified paths filter

--- scripts/helpers/getModifiedExamples.js
+++ scripts/helpers/getModifiedExamples.js
@@ -12,19 +12,23 @@ module.exports = () => {
   const deployAll = process.env.DEPLOY_ALL === 'true';
 
   // Get the list of modified files
-  const modifiedFiles = isMainBranch
-    ? // If the current branch is `main`, get the list of modified files from the last commit
-      execSync(
-        'git diff --name-only HEAD~1 | xargs -I {} dirname {} | sort | uniq'
-      )
-    : execSync(
-        'git diff --name-only origin/main | xargs -I {} dirname {} | sort | uniq'
-      )
-        .toString()
-        .split('\n');
+  const modifiedFiles = (
+    isMainBranch
+      ? // If the current branch is `main`, get the list of modified files from the last commit
+        execSync(
+          'git diff --name-only HEAD~1 | xargs -I {} dirname {} | sort | uniq'
+        )
+      : execSync(
+          'git diff --name-only origin/main | xargs -I {} dirname {} | sort | uniq'
+        )
+          .toString()
+          .split('\n')
+  ).filter(Boolean);
 
   // Filter the array of paths to return only the modified paths
   return examples.filter(
-    (path) => deployAll || modifiedFiles.includes(join(examplesPath, path))
+    (path) =>
+      deployAll ||
+      modifiedFiles.some((file) => file.includes(join(examplesPath, path)))
   );
 };
