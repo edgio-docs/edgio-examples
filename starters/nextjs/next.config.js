aefa3cea2581f801aba6aedbed6b0fb65280d1c0
Rishi Raj Jain <rjain@llnw.com>
2023-02-21 15:12:57 +0530

    Add next and nuxt starters that can be directly referenced for spinning up apps with Edgio

---
+++ starters/nextjs/next.config.js
@@ -0,0 +1,24 @@
+// This file was automatically added by edgio init.
+// You should commit this file to source control.
+const { withEdgio, withServiceWorker } = require('@edgio/next/config')
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+const _preEdgioExport = nextConfig;;
+
+module.exports = (phase, config) =>
+  withEdgio(
+    withServiceWorker({
+      // Output sourcemaps so that stack traces have original source filenames and line numbers when tailing
+      // the logs in the Edgio developer console.
+      edgioSourceMaps: true,
+
+      // Set the following to `true` to disable the Edgio dev tools.
+      disableEdgioDevTools: false,
+
+      ..._preEdgioExport
+    })
+  )
