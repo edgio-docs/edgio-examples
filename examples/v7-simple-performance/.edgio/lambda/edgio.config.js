5da32843ccd92033460543503ae2de8c3e0de42d
Tristan Lee <tristanlee85@gmail.com>
2023-04-07 11:14:02 -0400

    Add v7 simple example

---
+++ examples/v7-simple-performance/.edgio/lambda/edgio.config.js
@@ -0,0 +1,18 @@
+// edgio.config.js
+module.exports = {
+  origins: [
+    {
+      name: "origin",
+      override_host_header: "publicdomainreview.org",
+      hosts: [
+        {
+          location: {
+            hostname: "publicdomainreview.org"
+          }
+        }
+      ]
+    }
+  ],
+  name: "edgio-v7-simple-performance-example"
+};
+//# sourceMappingURL=edgio.config.js.map
---
+++ examples/v7-simple-performance/edgio.config.js
@@ -0,0 +1,21 @@
+// This file was automatically added by edgio init.
+// You should commit this file to source control.
+// Learn more about this file at https://docs.edg.io/guides/edgio_config
+module.exports = {
+  origins: [
+    {
+      name: 'origin',
+      override_host_header: 'publicdomainreview.org',
+      hosts: [
+        {
+          location: {
+            hostname: 'publicdomainreview.org',
+          },
+        },
+      ],
+    },
+  ],
+
+  // The name of the site in Edgio to which this app should be deployed.
+  name: 'edgio-v7-simple-performance-example',
+};
