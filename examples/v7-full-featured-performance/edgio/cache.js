b7e52d054d65799588922c4e1843c772db83d2c8
Tristan Lee <tristanlee85@gmail.com>
2023-04-10 12:47:28 -0400

    Changes to full feature example

--- examples/v7-full-featured-performance/edgio/cache.js
+++ examples/v7-full-featured-performance/edgio/cache.js
@@ -83,60 +83,56 @@ export const NEXT_CACHE_HANDLER = {
   },
 }
 
-export const SAMPLEAPIS_CACHE_HANDLER = ({ cache, proxy }) => {
-  cache({
-    edge: {
-      maxAgeSeconds: 60 * 60 * 24,
-      // Cache responses even if they contain cache-control: private header
-      // https://docs.edg.io/guides/caching#private
-      // https://docs.edg.io/docs/api/core/interfaces/_router_cacheoptions_.edgecacheoptions.html#forceprivatecaching
-      forcePrivateCaching: true,
-      staleWhileRevalidateSeconds: 60 * 60 * 24,
+export const SAMPLEAPIS_CACHE_HANDLER = {
+  caching: {
+    max_age: '86400s',
+    stale_while_revalidate: '86400s',
+    ignore_origin_no_cache: [200],
+    service_worker_max_age: '86400s',
+    bypass_client_cache: true,
+  },
+  headers: {
+    set_response_headers: {
+      'x-sw-cache-control': 'max-age=86400',
     },
-    browser: {
-      // Don't save the response in the browser
-      maxAgeSeconds: 0,
-      // Save the response in the browser via Edgio service worker
-      serviceWorkerSeconds: 60 * 60 * 24,
-      forcePrivateCaching: true,
-    },
-  })
-  proxy('sampleapis', {
-    path: ':path*',
-    transformResponse: (response) => {
-      // TODO response.body is not a string
-      // if (response.body) {
-      //   response.body = response.body.toString().replace(/https:\/\/m.media-amazon.com\//g, '/edgio-opt?quality=80&img=https://m.media-amazon.com/')
-      // }
-    },
-  })
+  },
+  origin: {
+    set_origin: 'sampleapis',
+  },
+  url: {
+    url_rewrite: [
+      {
+        source: '/edgio-sampleapis/:path*:optionalSlash(\\/?)?:optionalQuery(\\?.*)?',
+        syntax: 'path-to-regexp',
+        destination: '/:path*:optionalSlash:optionalQuery',
+      },
+    ],
+  },
 }
 
-export const IMDB_CACHE_HANDLER = ({ cache, proxy }) => {
-  cache({
-    edge: {
-      maxAgeSeconds: 60 * 60 * 24,
-      // Cache responses even if they contain cache-control: private header
-      // https://docs.edg.io/guides/caching#private
-      // https://docs.edg.io/docs/api/core/interfaces/_router_cacheoptions_.edgecacheoptions.html#forceprivatecaching
-      forcePrivateCaching: true,
-      staleWhileRevalidateSeconds: 60 * 60 * 24,
+export const IMDB_CACHE_HANDLER = {
+  caching: {
+    max_age: '86400s',
+    stale_while_revalidate: '86400s',
+    ignore_origin_no_cache: [200],
+    service_worker_max_age: '86400s',
+    bypass_client_cache: true,
+  },
+  headers: {
+    set_response_headers: {
+      'x-sw-cache-control': 'max-age=86400',
     },
-    browser: {
-      // Don't save the response in the browser
-      maxAgeSeconds: 0,
-      // Save the response in the browser via Edgio service worker
-      serviceWorkerSeconds: 60 * 60 * 24,
-      forcePrivateCaching: true,
-    },
-  })
-  proxy('imdb', {
-    path: ':path*',
-    transformResponse: (response) => {
-      // TODO response.body is not a string
-      // if (response.body) {
-      //   response.body = response.body.toString().replace('https://m.media-amazon.com/', '/edgio-opt?width=600&img=https://m.media-amazon.com/')
-      // }
-    },
-  })
+  },
+  origin: {
+    set_origin: 'imdb',
+  },
+  url: {
+    url_rewrite: [
+      {
+        source: '/edgio-imdb/:path*:optionalSlash(\\/?)?:optionalQuery(\\?.*)?',
+        syntax: 'path-to-regexp',
+        destination: '/:path*:optionalSlash:optionalQuery',
+      },
+    ],
+  },
 }
--- examples/v7-full-featured-performance/edgio/routes.js
+++ examples/v7-full-featured-performance/edgio/routes.js
@@ -15,7 +15,7 @@ router.match('/edgio-api/:path*', API_CACHE_HANDLER)
 router.match('/edgio-opt', IMAGE_CACHE_HANDLER)
 
 // Sample Api
-router.match('/:prefix?/edgio-sampleapis/:path*', SAMPLEAPIS_CACHE_HANDLER)
+router.match('/edgio-sampleapis/:path*', SAMPLEAPIS_CACHE_HANDLER)
 
 // imdb
 router.match('/edgio-imdb/:path*', IMDB_CACHE_HANDLER)
@@ -23,23 +23,23 @@ router.match('/edgio-imdb/:path*', IMDB_CACHE_HANDLER)
 // Sync rewrite as placed in next.config.js
 //router.get('/commerce/:name', ({ renderWithApp }) => renderWithApp())
 
-// Only compiled with 0 build / 0 deploy
-// if (isProductionBuild()) {
-//   // The data in Next.js comes through _next/data/project-build-id route.
-//   // For the route /product/product-slug, cache this SSR route's data
-//   // it on the edge so that can be prefetched
+// Only compiled with `edgio build` / `edgio deploy`
+if (isProductionBuild()) {
+  // The data in Next.js comes through _next/data/project-build-id route.
+  // For the route /product/product-slug, cache this SSR route's data
+  // it on the edge so that can be prefetched
 
-//   router.match('/_next/data/:path*', NEXT_CACHE_HANDLER)
+  router.match('/_next/data/:path*', NEXT_CACHE_HANDLER)
 
-//   // Cache but not in 0 dev mode
-//   router.match('/', NEXT_CACHE_HANDLER)
-//   router.match('/about', NEXT_CACHE_HANDLER)
-//   router.match('/commerce', NEXT_CACHE_HANDLER)
-//   router.match('/product/:name', NEXT_CACHE_HANDLER)
-//   router.match('/commerce/:name', NEXT_CACHE_HANDLER)
-//   router.match('/movies', NEXT_CACHE_HANDLER)
-//   router.match('/movie/:name', NEXT_CACHE_HANDLER)
-// }
+  // Cache but not in 0 dev mode
+  router.match('/', NEXT_CACHE_HANDLER)
+  router.match('/about', NEXT_CACHE_HANDLER)
+  router.match('/commerce', NEXT_CACHE_HANDLER)
+  router.match('/product/:name', NEXT_CACHE_HANDLER)
+  router.match('/commerce/:name', NEXT_CACHE_HANDLER)
+  router.match('/movies', NEXT_CACHE_HANDLER)
+  router.match('/movie/:name', NEXT_CACHE_HANDLER)
+}
 
 
 export default router
