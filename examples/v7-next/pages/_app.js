b83b2ed2bb2365c8cf73d3f30931de4fb7f49483
Rishi Raj Jain <rjain@llnw.com>
2023-05-10 03:23:04 +0530

    Update _app.js

--- examples/v7-next/pages/_app.js
+++ examples/v7-next/pages/_app.js
@@ -1,12 +1,12 @@
 import '@/styles/globals.css'
-import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import Navbar from '@/components/Navbar'
 import { useServiceWorker } from '@edgio/react'
 
 const MyApp = ({ Component, pageProps }) => {
   const router = useRouter()
-  useServiceWorker()
+  // Install the service worker to enable prefetching
+  useServiceWorker({ dev: false /* set to true to install the service worker in development mode */ })
   return (
     <div className="flex min-h-screen flex-col bg-gradient-to-br from-[#9a1ab1] via-[#004966] to-[#01B18D]">
       <Navbar />
