55e967b6670ac487451edaf1c7ac4d193dc0a90a
Rishi Raj Jain <rjain@llnw.com>
2023-05-10 03:20:23 +0530

    Update _app.js

--- examples/v7-next/pages/_app.js
+++ examples/v7-next/pages/_app.js
@@ -1,9 +1,14 @@
 import '@/styles/globals.css'
+import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import Navbar from '@/components/Navbar'
+import { useServiceWorker } from '@edgio/react'
 
 const MyApp = ({ Component, pageProps }) => {
   const router = useRouter()
+  useEffect(() => {
+    useServiceWorker()
+  }, [])
   return (
     <div className="flex min-h-screen flex-col bg-gradient-to-br from-[#9a1ab1] via-[#004966] to-[#01B18D]">
       <Navbar />
