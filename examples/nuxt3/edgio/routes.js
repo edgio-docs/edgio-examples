7a1f1d073780a53c8af7330d92adf349caab1d97
Jakub Jarek <j.jarek@outlook.com>
2023-06-21 12:35:35 +0200

    cleanup

--- examples/nuxt3/edgio/routes.js
+++ examples/nuxt3/edgio/routes.js
@@ -1,6 +1,6 @@
 import { Router } from '@edgio/core'
 import { nuxtRoutes } from '@edgio/nuxt-nitro'
-// import getPrerenderRequests from './prerenderRequests'
+import getPrerenderRequests from './prerenderRequests'
 import { isProductionBuild } from '@edgio/core/environment'
 import { API_CACHE_HANDLER, IMAGE_CACHE_HANDLER, EDGE_CACHE_HANDLER } from './cache'
 
@@ -14,7 +14,7 @@ router.use(nuxtRoutes)
 // the set of links are visited to warm the cache
 // for future visits (expected to be the first view for real users)
 // More on static prerendering: https://docs.edg.io/guides/static_prerendering
-// router.prerender(getPrerenderRequests)
+router.prerender(getPrerenderRequests)
 
 // API (Any backend) caching
 router.match('/l0-api/:path*', API_CACHE_HANDLER)
@@ -22,9 +22,9 @@ router.match('/l0-api/:path*', API_CACHE_HANDLER)
 // Image caching
 router.match('/l0-opt', IMAGE_CACHE_HANDLER)
 
-// Only compiled with 0 build / 0 deploy
+// Only compiled with edgio build / edgio deploy
 if (isProductionBuild()) {
-  // Cache but not in 0 dev mode
+  // Cache but not in edgio dev mode
   router.match('/', EDGE_CACHE_HANDLER)
   router.match('/about', EDGE_CACHE_HANDLER)
   router.match('/commerce', EDGE_CACHE_HANDLER)
