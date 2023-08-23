49d89cacc270770c08d4db981e2c3326b31eb4d7
Tristan Lee <tristan.lee@moovweb.com>
2023-08-23 10:57:54 -0400

    Add caching

--- examples/v7-vuejs/routes.ts
+++ examples/v7-vuejs/routes.ts
@@ -2,6 +2,15 @@
 // You should commit this file to source control.
 
 import { Router } from '@edgio/core'
+import type { Features } from '@edgio/core/types'
 import { vueRoutes } from '@edgio/vue-cva'
 
-export default new Router().use(vueRoutes)
+const defaultFeatures: Features = {
+  caching: {
+    max_age: '1d',
+    stale_while_revalidate: '1d',
+    service_worker_max_age: '1h'
+  }
+}
+
+export default new Router().match('/(.*)', defaultFeatures).use(vueRoutes)
