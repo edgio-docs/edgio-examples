5bc1c2b517178ad06d07d94adefe89d1392e1925
Tristan Lee <tristanlee85@gmail.com>
2023-10-05 11:17:15 -0400

    Latest Astro support

--- examples/v7-astro/routes.ts
+++ examples/v7-astro/routes.ts
@@ -1,15 +1,6 @@
 // This file was automatically added by edgio init.
 // You should commit this file to source control.
-import { Router } from '@edgio/core';
-import type { Features } from '@edgio/core/types';
-import { astroRoutes } from '@edgio/astro';
+import { Router } from '@edgio/core'
+import { astroRoutes } from '@edgio/astro'
 
-const defaultFeatures: Features = {
-  caching: {
-    max_age: '1d',
-    stale_while_revalidate: '1d',
-    service_worker_max_age: '1h',
-  },
-};
-
-export default new Router().match('/(.*)', defaultFeatures).use(astroRoutes);
+export default new Router().use(astroRoutes)
