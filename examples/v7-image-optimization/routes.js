dba2ad1ea440328db3e979c22ade0b43b4882907
Tristan Lee <tristanlee85@gmail.com>
2023-10-24 13:23:42 -0400

    Base project for image opt

---
+++ examples/v7-image-optimization/routes.js
@@ -0,0 +1,8 @@
+// This file was added by edgio init.
+// You should commit this file to source control.
+import { Router, edgioRoutes } from '@edgio/core';
+
+export default new Router()
+
+  // plugin enabling basic Edgio functionality
+  .use(edgioRoutes);
