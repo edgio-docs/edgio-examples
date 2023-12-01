147b53a61a7be5d9fdf6324bd87a379f6c7efcba
Tristan Lee <tristanlee85@gmail.com>
2023-12-01 13:22:46 -0500

    Expand example to show before/after edge function modification

--- examples/v7-ef-cloud-fetch/src/app/page.tsx
+++ examples/v7-ef-cloud-fetch/src/app/page.tsx
@@ -1,25 +1,19 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h2 className="max-w-5xl text-left">
+        Welcome! This page demonstrates how Edge Functions can be used with
+        Next.js to modify the cloud function responses at the edge. By visiting
+        the link below, the following request flow will occur:
+      </h2>
       <div className="z-10 max-w-5xl w-full text-center font-mono text-sm">
-        <p className="mb-4 text-left mx-auto">
-          Welcome! This page demonstrates how Edgio Edge Functions can be used
-          with Next.js to modify cloud function responses at the edge. By
-          visiting the link below, the following request flow will occur:
-        </p>
         <ol className="list-decimal list-inside text-left my-4">
           <li>
-            The browser will request the page{' '}
-            <code className="bg-gray-700 p-1 rounded">/edge-override</code>.
+            The browser will request the page <pre>/example</pre>.
           </li>
           <li>
-            Edgio will match this request as defined in the{' '}
-            <code className="bg-gray-700 p-1 rounded">routes.ts</code> file and
-            process it through{' '}
-            <code className="bg-gray-700 p-1 rounded">
-              ./edge-functions/index.js
-            </code>
-            .
+            Edgio will match this request as defined in the <pre>routes.ts</pre>{' '}
+            file and process it through <pre>./edge-functions/index.js</pre>.
           </li>
           <li>
             The edge function will request the page from the Next.js server
@@ -30,22 +24,32 @@ export default function Home() {
             function.
           </li>
           <li>
-            The edge function will modify the response and send it to the
-            browser.
+            The edge function will modify the response, replacing{' '}
+            <pre>Edgio Cloud Functions</pre> with{' '}
+            <pre>Edgio Edge Functions</pre>, and send it to the browser.
           </li>
         </ol>
+      </div>
+
+      <div className="text-center font-mono text-sm flex items-center justify-between space-x-4">
         <a
-          href="/edge-override"
+          href="/example"
           className="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         >
-          Go to /edge-override
+          Go to <pre>/example</pre> (no edge function process)
+        </a>
+        <a
+          href="/example?edge=1"
+          className="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Go to <pre>/example?edge=1</pre> (edge function process)
         </a>
       </div>
-      <footer className="z-0 absolute bottom-0 left-0 right-0 bg-gray-800 text-gray-100 text-center text-xs py-2">
+      <div className="text-center text-md mt-4">
         <a href="https://github.com/edgio-docs/edgio-v7-ef-cloud-fetch-example">
           Example Source Code
         </a>
-      </footer>
+      </div>
     </main>
   );
 }
