64e674d9428b1c41b09d7d549f15c57acd2a31a8
Tristan Lee <tristanlee85@gmail.com>
2024-02-02 13:27:11 -0500

    New astro app

--- examples/v7-astro/routes.ts
+++
@@ -1,6 +0,0 @@
-// This file was automatically added by edgio init.
-// You should commit this file to source control.
-import { Router } from '@edgio/core'
-import { astroRoutes } from '@edgio/astro'
-
-export default new Router().use(astroRoutes)
